8d5f051ae067abaca6639d52f78d58f603060d98
ArezouFarshi <arezou.farshi@mail.polimi.it>
2025-11-12 15:58:38 +0100

    Update PanelEventAddedDTO.cs

--- PanelEventAddedDTO.cs
+++ PanelEventAddedDTO.cs
@@ -5,12 +5,11 @@ using Nethereum.Contracts;
 [Event("PanelEventAdded")]
 public class PanelEventAddedDTO : IEventDTO
 {
-    [Parameter("string",  "panelId",      1, false)] public string? PanelId { get; set; }
-    [Parameter("string",  "eventType",    2, false)] public string? EventType { get; set; }
-    [Parameter("string",  "faultType",    3, false)] public string? FaultType { get; set; }
-    [Parameter("string",  "faultSeverity",4, false)] public string? FaultSeverity { get; set; }
-    [Parameter("string",  "actionTaken",  5, false)] public string? ActionTaken { get; set; }
-    [Parameter("bytes32", "eventHash",    6, false)] public byte[]? EventHash { get; set; }
-    [Parameter("address", "validatedBy",  7, false)] public string? ValidatedBy { get; set; }
-    [Parameter("uint256", "timestamp",    8, false)] public BigInteger Timestamp { get; set; }
+    [Parameter("string", "panelId", 1, false)] public string? PanelId { get; set; }
+    [Parameter("bool", "ok", 2, false)] public bool Ok { get; set; }
+    [Parameter("string", "color", 3, false)] public string? Color { get; set; }
+    [Parameter("string", "status", 4, false)] public string? Status { get; set; }
+    [Parameter("int256", "prediction", 5, false)] public BigInteger Prediction { get; set; }
+    [Parameter("string", "reason", 6, false)] public string? Reason { get; set; }
+    [Parameter("uint256", "timestamp", 7, false)] public BigInteger Timestamp { get; set; }
 }
