984288ec6c26fc85de699de2df631089a17a8ab5
ArezouFarshi <arezou.farshi@mail.polimi.it>
2025-11-12 16:13:05 +0100

    Update PanelEventAddedDTO.cs

--- PanelEventAddedDTO.cs
+++ PanelEventAddedDTO.cs
@@ -5,11 +5,11 @@ using Nethereum.Contracts;
 [Event("PanelEventAdded")]
 public class PanelEventAddedDTO : IEventDTO
 {
-    [Parameter("string", "panelId", 1, false)] public string? PanelId { get; set; }
-    [Parameter("bool", "ok", 2, false)] public bool Ok { get; set; }
-    [Parameter("string", "color", 3, false)] public string? Color { get; set; }
-    [Parameter("string", "status", 4, false)] public string? Status { get; set; }
-    [Parameter("int256", "prediction", 5, false)] public BigInteger Prediction { get; set; }
-    [Parameter("string", "reason", 6, false)] public string? Reason { get; set; }
+    [Parameter("string",  "panelId",   1, false)] public string? PanelId { get; set; }
+    [Parameter("bool",    "ok",        2, false)] public bool Ok { get; set; }
+    [Parameter("string",  "color",     3, false)] public string? Color { get; set; }
+    [Parameter("string",  "status",    4, false)] public string? Status { get; set; }
+    [Parameter("int256",  "prediction",5, false)] public BigInteger Prediction { get; set; }
+    [Parameter("string",  "reason",    6, false)] public string? Reason { get; set; }
     [Parameter("uint256", "timestamp", 7, false)] public BigInteger Timestamp { get; set; }
 }
